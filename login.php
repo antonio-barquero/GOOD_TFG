4eb49f7ec1a5adc8b1df403e183f68fa0ec3a5d9
antonio-barquero <antoniobarquero6216@gmail.com>
2021-05-05 12:24:32 +0200

    quitado js

--- login.php
+++ login.php
@@ -1,46 +1,38 @@
 <!Doctype html>
 <html>
-<head>
-  <meta charset="UTF-8">
-	<title>Login</title>
-	<link rel="stylesheet" type="text/css" href="login.css">
-</head>
-<body>
-<?php 
+  <head>
+    <meta charset="UTF-8">
+	  <title>Login Wallacar</title>
+	  
+    <link href="login.css" rel="stylesheet" type="text/css">
+  
+
+  </head>
+  <body>
+      <?php 
          include 'menu.html' ;
        ?>
        <br><br><br><br>
-	
-<link href="login.css" rel="stylesheet">
+    
 
-<div class="testbox">
-  <h1>Registration</h1>
+       <p class="texto" >Registro</p>
+        <div class="Registro">
+          <form method="post" action="">
 
-  <form action="/">
-      <hr>
-    <div class="accounttype">
-      <input type="radio" value="None" id="radioOne" name="account" checked/>
-      <label for="radioOne" class="radio" chec>Personal</label>
-      <input type="radio" value="None" id="radioTwo" name="account" />
-      <label for="radioTwo" class="radio">Concesionario</label>
-      
-    </div>
-  <hr>
-  <label id="icon" for="name"><i class="icon-envelope "></i></label>
-  <input type="text" name="name" id="name" placeholder="Email" required/>
-  <label id="icon" for="name"><i class="icon-user"></i></label>
-  <input type="text" name="name" id="name" placeholder="Nombre" required/>
-  <label id="icon" for="name"><i class="icon-shield"></i></label>
-  <input type="password" name="name" id="name" placeholder="Contraseña" required/>
-  
-   <p>By clicking Register, you agree on our <a href="#">terms and condition</a>.</p>
-   <a href="#" class="button">Registro</a>
-  </form>
-</div>
+            <input type="text" required placeholder="Nombre de usuario" autocomplete="off"> 
+            <input type="text" id="email" required placeholder="Correo" autocomplete="off">
+            <input type="password" name="password" id="password" required placeholder="Contraseña" autocomplete="off"> 
+            <input type="submit" value="Registrar" title="Registra tu cuenta">
+        </div> 
 
-<?php 
+
+
+
+
+   </body>
+        <?php 
          include 'menu2.html' ;
        ?>
-  </body>
+  
 
-  </html>
+ </html>
